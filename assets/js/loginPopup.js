c560a4340794922d7ad31ee23c37c67e4b260d23
byeopsi <4217609@gmail.com>
2026-01-06 12:23:38 +0900

    스타일) 미디어 쿼리 추가

--- assets/js/loginPopup.js
+++ assets/js/loginPopup.js
@@ -1,12 +1,15 @@
 // 로그인 모달 스크립트
 let modal = false;
-const loginPopup = document.querySelector("#login-popup");
-const loginContainer = document.querySelector("#login-container");
-loginPopup.addEventListener("click",
-  (e) => {if(e.target === loginPopup){toggleLoginPopup()}});
+const loginPopup = document.querySelector('#login-popup');
+const loginContainer = document.querySelector('#login-container');
+loginPopup.addEventListener('click', e => {
+  if (e.target === loginPopup) {
+    toggleLoginPopup();
+  }
+});
 
 const toggleLoginPopup = () => {
-  if(!modal) {
+  if (!modal) {
     loginPopup.style.zIndex = 101;
     loginPopup.style.opacity = 1;
     modal = true;
@@ -17,4 +20,4 @@ const toggleLoginPopup = () => {
     }, 250);
     modal = false;
   }
-}
+};
